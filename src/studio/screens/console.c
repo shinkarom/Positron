ebfe734e81cdade4e8cab4c83bf6d61324d518c9
shinkarom <shinkarom@gmail.com>
2024-03-30 13:53:29 +0200

    Remove native export

--- src/studio/screens/console.c
+++ src/studio/screens/console.c
@@ -104,12 +104,6 @@
     macro(bpp)
 
 #define EXPORT_CMD_LIST(macro)  \
-    macro(win)                  \
-    macro(winxp)                \
-    macro(linux)                \
-    macro(rpi)                  \
-    macro(mac)                  \
-    macro(html)                 \
     macro(binary)               \
     macro(tiles)                \
     macro(sprites)              \
@@ -2055,142 +2049,6 @@ typedef struct
     char filename[TICNAME_MAX];
 } GameExportData;
 
-static void onExportGet(const net_get_data* data)
-{
-    GameExportData* exportData = (GameExportData*)data->calldata;
-    Console* console = exportData->console;
-
-    switch(data->type)
-    {
-    case net_get_progress:
-        {
-            console->cursor.pos.x = 0;
-            printf("\r");
-            printBack(console, "GET ");
-            printFront(console, data->url);
-
-            char buf[8];
-            sprintf(buf, " [%i%%]", data->progress.size * 100 / data->progress.total);
-            printBack(console, buf);
-        }
-        break;
-    case net_get_error:
-        printError(console, "file downloading error :(");
-        commandDone(console);
-        free(exportData);
-        break;
-    default:
-        break;
-    }
-}
-
-static void onNativeExportGet(const net_get_data* data)
-{
-    switch(data->type)
-    {
-    case net_get_done:
-        {
-            GameExportData* exportData = (GameExportData*)data->calldata;
-            Console* console = exportData->console;
-
-            tic_mem* tic = console->tic;
-
-            char filename[TICNAME_MAX];
-            strcpy(filename, exportData->filename);
-            free(exportData);
-
-            s32 size = data->done.size;
-
-            printLine(console);
-
-            const char* path = tic_fs_path(console->fs, filename);
-            void* buf = NULL;
-
-            onFileExported(console, filename, (buf = embedCart(console, data->done.data, &size)) && fs_write(path, buf, size));
-            chmod(path, DEFAULT_CHMOD);
-
-            if (buf)
-                free(buf);
-        }
-        break;
-    default:
-        onExportGet(data);
-    }
-}
-
-static void exportGame(Console* console, const char* name, const char* system, net_get_callback callback, ExportParams params)
-{
-    tic_mem* tic = console->tic;
-    printLine(console);
-    GameExportData data = {console};
-    strcpy(data.filename, name);
-
-    char url[TICNAME_MAX] = "/export/" DEF2STR(TIC_VERSION_MAJOR) "." DEF2STR(TIC_VERSION_MINOR) TIC_VERSION_STATUS "/";
-    strcat(url, system);
-
-#if defined(TIC80_PRO)
-    if (params.alone)
-        strcat(url, tic_core_script_config(console->tic)->name);
-#endif
-
-    tic_net_get(console->net, url, callback, MOVE(data));
-}
-
-static inline void exportNativeGame(Console* console, const char* name, const char* system, ExportParams params)
-{
-    exportGame(console, name, system, onNativeExportGet, params);
-}
-
-static void onHtmlExportGet(const net_get_data* data)
-{
-    switch(data->type)
-    {
-    case net_get_done:
-        {
-            GameExportData* exportData = (GameExportData*)data->calldata;
-            Console* console = exportData->console;
-
-            tic_mem* tic = console->tic;
-
-            char filename[TICNAME_MAX];
-            strcpy(filename, exportData->filename);
-            free(exportData);
-
-            const char* zipPath = tic_fs_path(console->fs, filename);
-            bool errorOccurred = !fs_write(zipPath, data->done.data, data->done.size);
-
-            if(!errorOccurred)
-            {
-                struct zip_t *zip = zip_open(zipPath, ZIP_DEFAULT_COMPRESSION_LEVEL, 'a');
-
-                if(zip) SCOPE(zip_close(zip))
-                {
-                    void* cart = newCart();
-
-                    SCOPE(free(cart))
-                    {
-                        s32 cartSize = tic_cart_save(&tic->cart, cart);
-
-                        if(cartSize)
-                        {
-                            zip_entry_open(zip, "cart.tic");
-                            zip_entry_write(zip, cart, cartSize);
-                            zip_entry_close(zip);
-                        }
-                        else errorOccurred = true;
-                    }
-                }
-                else errorOccurred = true;
-            }
-
-            onFileExported(console, filename, !errorOccurred);
-        }
-        break;
-    default:
-        onExportGet(data);
-    }
-}
-
 static const char* getFilename(const char* filename, const char* ext)
 {
     if(strcmp(filename + strlen(filename) - strlen(ext), ext) == 0)
@@ -2201,36 +2059,6 @@ static const char* getFilename(const char* filename, const char* ext)
     strcat(Name, ext);
 
     return Name;
-}
-
-static void onExport_win(Console* console, const char* param, const char* filename, ExportParams params)
-{
-    exportNativeGame(console, getFilename(filename, ".exe"), param, params);
-}
-
-static void onExport_winxp(Console* console, const char* param, const char* filename, ExportParams params)
-{
-    exportNativeGame(console, getFilename(filename, ".exe"), param, params);
-}
-
-static void onExport_linux(Console* console, const char* param, const char* filename, ExportParams params)
-{
-    exportNativeGame(console, filename, param, params);
-}
-
-static void onExport_rpi(Console* console, const char* param, const char* filename, ExportParams params)
-{
-    exportNativeGame(console, filename, param, params);
-}
-
-static void onExport_mac(Console* console, const char* param, const char* filename, ExportParams params)
-{
-    exportNativeGame(console, filename, param, params);
-}
-
-static void onExport_html(Console* console, const char* param, const char* filename, ExportParams params)
-{
-    exportGame(console, getFilename(filename, ".zip"), param, onHtmlExportGet, params);
 }
 
 static void onExport_tiles(Console* console, const char* param, const char* filename, ExportParams params)
@@ -2914,8 +2742,6 @@ static const char HelpUsage[] = "help [<text>"
                                                                                         \
     macro("export",                                                                     \
         NULL,                                                                           \
-        "export cart to HTML,\n"                                                        \
-        "native build (win linux rpi mac),\n"                                           \
         "export sprites/map/... as a .png image "                                       \
         "or export sfx and music to .wav files.",                                       \
         "\nexport [" EXPORT_CMD_LIST(EXPORT_CMD_DEF) "...] "                            \
